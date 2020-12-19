79a5c5f3a92b35bf4dc0d0f2b1c2bf2e8bdd7e96
Muhammad Hamza Farrukh <hamzafarrukh141@gmail.com>
2020-12-19 01:38:18 -0500

    Day 19 - cheated??? by giving up and using a parsing library

---
+++ d19/part2.py
@@ -0,0 +1,55 @@
+import re
+from collections import deque
+import sys
+from copy import copy, deepcopy
+from collections import defaultdict
+from lark import Lark
+
+
+def readFile(file):
+    with open(file, 'r') as f:
+        return f.readlines()
+
+
+lines = readFile('E:\\Projects\\AdventOfCode2020\\d19\\input.txt')
+
+i = 0
+rules = ''
+inputs = set()
+
+while i < len(lines):
+    l = lines[i].strip()
+
+    if ':' in l:
+        rules += l + '\n'
+    elif len(l) > 0:
+        inputs.add(l)
+
+    i += 1
+
+rules = rules.replace('8: 42', '8: 42 | 42 8')
+rules = rules.replace('11: 42 31', '11: 42 31 | 42 11 31')
+
+before = 0
+after = ord('a')
+for i in range(26):
+    rules = rules.replace(str(before), chr(after))
+    before += 1
+    after += 1
+
+l = Lark(f'''
+    {rules}
+    %import common.WORD   // imports from terminal library
+    %ignore " "           // Disregard spaces in text
+    '''
+         )
+
+match = 0
+for i in inputs:
+    try:
+        l.parse(i)
+        match += 1
+    except Exception:
+        pass
+
+print(match)
